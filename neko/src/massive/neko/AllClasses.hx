ffe5ce324032e9a16e47c964d562bac58ec67626
Mike Stead <mike.stead@massive.com.au>
2010-12-24 14:32:01 +1100

    update license format

--- neko/src/massive/neko/AllClasses.hx
+++ neko/src/massive/neko/AllClasses.hx
@@ -25,70 +25,24 @@
  * authors and should not be interpreted as representing official policies, either expressed
  * or implied, of Massive Interactive.
  */
-package massive.neko.util;
+package massive.neko;
 
-import neko.Lib;
-import massive.neko.io.FileSys;
-import neko.io.Path;
-
-import neko.zip.Reader;
-import neko.zip.Writer;
-import haxe.io.Bytes;
-
+import massive.neko.AllClasses;
+import massive.neko.cmd.Command;
+import massive.neko.cmd.CommandLineRunner;
+import massive.neko.cmd.Console;
+import massive.neko.cmd.ICommand;
+import massive.neko.haxe.HaxeWrapper;
+import massive.neko.haxelib.Haxelib;
+import massive.neko.haxelib.HaxelibTools;
 import massive.neko.io.File;
-import neko.FileSystem;
+import massive.neko.io.FileError;
+import massive.neko.io.FileSys;
+import massive.neko.util.PathUtil;
+import massive.neko.util.ZipUtil;
 
-class ZipUtil
+class AllClasses
 {
-	public static function zipDirectory(file:File, sourceDir:File, ?filter:EReg, ?exclude:Bool=false):Void
-	{
-		if(!sourceDir.exists || !sourceDir.isDirectory) throw "Error: source isn't a valid directory";
-		
-		// set cwd to directory path
-		
-		var old:File = File.current;
-		
-		var pwd:String = FileSys.getCwd();
-		
-		FileSys.setCwd(sourceDir.nativePath);
-		
-		var cwd:String = FileSys.getCwd();
-
-		// get entries
-		var entries:Array<Dynamic> = convertDirectoryToZipEntries(sourceDir, filter, exclude);
-		// zip entries
-		
-		if(!file.exists)
-		{
-			file.writeString("");
-		}
-		
-		var zip = neko.io.File.write(file.nativePath, true);
-		neko.zip.Writer.writeZip(zip, entries, -1);
-		zip.close();
-		
-		// return to previous working directory
-		FileSys.setCwd(pwd);
-
-	}
-	
-	public static function convertDirectoryToZipEntries(dir:File, ?filter:EReg, ?exclude:Bool=false):Array<Dynamic>
-	{
-		var files:Array<File> = dir.getRecursiveDirectoryListing(filter, exclude);
-
-		var entries:Array<Dynamic> = [];
-			
-		for (file in files)
-		{
-			var bytes:Bytes = file.isDirectory ? null: neko.io.File.getBytes(file.nativePath);
-			var stat:FileStat = FileSys.stat(file.nativePath);
-			var name:String = dir.getRelativePath(file) + (file.isDirectory ? File.seperator : "");
-			var entry = { fileTime:stat.mtime, fileName:name, data:bytes };
-			
-			//neko.Lib.print("Added " + entry.fileName + "\n");
-			entries.push(entry);
-		}
-		
-		return entries;
-	}
+	public static function main():AllClasses {return new AllClasses();}
+	public function new(){trace('This is a generated main class');}
 }
--- tool/src/massive/mlib/AllClasses.hx
+++ tool/src/massive/mlib/AllClasses.hx
@@ -25,70 +25,23 @@
  * authors and should not be interpreted as representing official policies, either expressed
  * or implied, of Massive Interactive.
  */
-package massive.neko.util;
+package massive.mlib;
 
-import neko.Lib;
-import massive.neko.io.FileSys;
-import neko.io.Path;
+import massive.mlib.AllClasses;
+import massive.mlib.cmd.ConfigMlibCommand;
+import massive.mlib.cmd.GenerateAllClassesCommand;
+import massive.mlib.cmd.IncrementHaxelibVersionCommand;
+import massive.mlib.cmd.InstallToHaxelibCommand;
+import massive.mlib.cmd.MlibCommand;
+import massive.mlib.cmd.PackageForHaxelibCommand;
+import massive.mlib.cmd.PreProcessHxmlCommand;
+import massive.mlib.cmd.SubmitToHaxelibCommand;
+import massive.mlib.cmd.UpdateSourceLicenseCommand;
+import massive.mlib.Mlib;
+import massive.mlib.MlibSettings;
 
-import neko.zip.Reader;
-import neko.zip.Writer;
-import haxe.io.Bytes;
-
-import massive.neko.io.File;
-import neko.FileSystem;
-
-class ZipUtil
+class AllClasses
 {
-	public static function zipDirectory(file:File, sourceDir:File, ?filter:EReg, ?exclude:Bool=false):Void
-	{
-		if(!sourceDir.exists || !sourceDir.isDirectory) throw "Error: source isn't a valid directory";
-		
-		// set cwd to directory path
-		
-		var old:File = File.current;
-		
-		var pwd:String = FileSys.getCwd();
-		
-		FileSys.setCwd(sourceDir.nativePath);
-		
-		var cwd:String = FileSys.getCwd();
-
-		// get entries
-		var entries:Array<Dynamic> = convertDirectoryToZipEntries(sourceDir, filter, exclude);
-		// zip entries
-		
-		if(!file.exists)
-		{
-			file.writeString("");
-		}
-		
-		var zip = neko.io.File.write(file.nativePath, true);
-		neko.zip.Writer.writeZip(zip, entries, -1);
-		zip.close();
-		
-		// return to previous working directory
-		FileSys.setCwd(pwd);
-
-	}
-	
-	public static function convertDirectoryToZipEntries(dir:File, ?filter:EReg, ?exclude:Bool=false):Array<Dynamic>
-	{
-		var files:Array<File> = dir.getRecursiveDirectoryListing(filter, exclude);
-
-		var entries:Array<Dynamic> = [];
-			
-		for (file in files)
-		{
-			var bytes:Bytes = file.isDirectory ? null: neko.io.File.getBytes(file.nativePath);
-			var stat:FileStat = FileSys.stat(file.nativePath);
-			var name:String = dir.getRelativePath(file) + (file.isDirectory ? File.seperator : "");
-			var entry = { fileTime:stat.mtime, fileName:name, data:bytes };
-			
-			//neko.Lib.print("Added " + entry.fileName + "\n");
-			entries.push(entry);
-		}
-		
-		return entries;
-	}
+	public static function main():AllClasses {return new AllClasses();}
+	public function new(){trace('This is a generated main class');}
 }
